396f09170c4bd83cd4d78f347cfa8c2f295fa1e7
John Canning <jmcanning@alum.mit.edu>
2017-06-28 11:58:59 -0700

    Modified home page (home2.html) with baseline content for mobile and desktop
    The home page now has content for 3 screens: Home, Services, and Contact Us.
    The contact information is in the form of graphics that can't easily be
    collected by spiders.  Accompanying javascript and stylesheets define the
    behavior and layout.

---
+++ public_html/main-functions.js
@@ -0,0 +1,38 @@
+function clear_choices() {
+    var choices = document.getElementsByClassName("chosen");
+    for (var i = 0; i < choices.length; i++) {
+	if (choices[i].id) {
+/*	    alert('Setting ' + choices[i].id + '.display = "none"'); */
+	    choices[i].style.display = "none";
+	}
+    }
+}
+function display_choice(id) {
+    clear_choices();
+/*    alert('Setting ' + id + '.display = "block"'); */
+    document.getElementById(id).style.display = "block";
+}
+function send_info_email() {
+    var prefixes = ['www.' ];
+    var host = window.location.hostname;
+    if (host == '') {
+	host = "localhost"
+    }
+    for (var i=0; i < prefixes.length; i++) {
+	prefix = prefixes[i]
+	if (host.substring(0,prefix.length) == prefix) {
+	    host = host.slice(prefix.length)
+	}
+    }
+    var mailloc = 'mailto' + ':' + 'info@' + host;
+    window.location.assign(mailloc);
+}
+function call_info_phone() {
+    var seq = '0380';
+    var areacode = '(971)-';
+    var exchange = '251-';
+    var uri = 'tel:' + '+1' + areacode + exchange + seq;
+    window.location.assign(uri);
+}
+
+
